c3c4a353b2ab382be3e63a09b5c77cb3b199e9d3
kristian <noemail@parrot>
2017-05-05 16:28:11 +0200

    Grabber functional

--- WRO_2017/src/wro/_2017/core/WRO.java
+++ WRO_2017/src/wro/_2017/core/WRO.java
@@ -1,5 +1,7 @@
 package wro._2017.core;
 
+import java.util.ArrayList;
+
 //import java.sql.Driver; <-----
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.ev3.LocalEV3;
@@ -13,7 +15,7 @@ public class WRO {
 		EV3 ev3 = LocalEV3.get();
 		Robot robot = new Robot(ev3);
 		robot.init();
-		Driver driver = new Driver(robot);
+		Driver driver = new Driver(robot,new ArrayList<Waypoint>(), 111,118,0);
 		
 		
 //		while(robot.getMovementControler().isMoving()){}
@@ -34,14 +36,21 @@ public class WRO {
 //		try {
 //			SVGMapLoader loader = new SVGMapLoader(new FileInputStream("map2.svg"));
 //			LineMap MAP = loader.readLineMap();
-		
-		driver.getPath().add(new Waypoint(0 , 45));
-		driver.getPath().add(new Waypoint(44, 45));
-		driver.getPath().add(new Waypoint(44, 23));
-		
 //		
-		driver.follow();
-			
+//		driver.getPath().add(new Waypoint(0 , 45));
+//		driver.getPath().add(new Waypoint(44, 45));
+//		driver.getPath().add(new Waypoint(44, 23));
+//		
+//		driver.getPath().add(new Waypoint(58.66,118.3));
+//		driver.getPath().add(new Waypoint(58.26,185.61));
+//		
+//		driver.follow();
+		Grabber g = new Grabber(robot.getMotor_A(), robot.getMotor_D());
+		g.top();
+		g.bottom();
+		
+		g.moveTo(0.4f);
+		g.bottom();
 //		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 //			e.printStackTrace();
--- WRO_2017/src/wro/_2017/grabber/Grabber.java
+++ WRO_2017/src/wro/_2017/grabber/Grabber.java
@@ -1,10 +1,12 @@
 package wro._2017.grabber;
 
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.robotics.LinearActuator;
 
 public class Grabber {
 	private EV3MediumRegulatedMotor vert, grab;
-
+	private float linearPos = 0;
+	private float MAX_ROT = 6.5f;
 	public Grabber(EV3MediumRegulatedMotor vert, EV3MediumRegulatedMotor grab) {
 		super();
 		this.vert = vert;
@@ -19,6 +21,24 @@ public class Grabber {
 		grab.rotate(-rot);
 	}
 	
+	public void top(){
+		vert.rotate((int) ((MAX_ROT-linearPos)*360));
+		
+		linearPos = MAX_ROT;
+	}
+	
+	public void moveTo(float percent){
+		float pos = (MAX_ROT*percent);
+		float rotations = pos-linearPos;
+		vert.rotate((int) (rotations*360));
+		linearPos = pos;
+	}
+	
+	
+	public void bottom(){
+		vert.rotate((int) (-linearPos*360));
+		linearPos = 0;
+	}
 	public EV3MediumRegulatedMotor getVert() {
 		return vert;
 	}
